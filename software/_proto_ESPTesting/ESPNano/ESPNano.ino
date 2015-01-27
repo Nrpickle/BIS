a5429f7eee4b284dacce761f9b134470d0336af3
Nick McComb <nick@nickmccomb.net>
2015-01-26 17:38:12 -0800

    Achieved communication with ESP8266!

--- software/_proto_ESPTesting/ESPNano/ESPNano.ino
+++ software/_proto_ESPTesting/ESPNano/ESPNano.ino
@@ -4,18 +4,18 @@
 #define PASS    "27304600"
 #define DST_IP  "220.181.111.85"
 
-SoftwareSerial dbgSerial(11, 10);
+SoftwareSerial dbgSerial(11, 10); //Rx, Tx
 
 void setup(){
 	//Open serial communication
-	Serial.begin(115200);
+	Serial.begin(9600);
 	Serial.setTimeout(5000);
 	
-	dbgSerial.begin(57600);
+	dbgSerial.begin(9600);
 	dbgSerial.println("ESP8266 Demo");
 	
 	//Test if the module is ready
-	Serial.println("AT");
+	Serial.print("AT\015\012");
 	delay(2000);
 	if(Serial.available())
 	{
@@ -30,6 +30,6 @@ void setup(){
 
 void loop(){
 	if(Serial.available()){
-		dbgSerial.print(Serial.read(), DEC);
+		dbgSerial.print((char) Serial.read());
 	}
 }
